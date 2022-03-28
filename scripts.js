d842f887b2346a2a29e9b576aaa2dd86881cd903
MagicToasterMan <88402889+eclar971@users.noreply.github.com>
2022-03-28 13:32:58 -0700

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -114,7 +114,7 @@ window.addEventListener("keydown", function movement(obj) {
 	t = blockTop;
 	var myCanvasEl = document.getElementById("myCanvas");
 	var ctx = myCanvasEl.getContext("2d");
-	myCanvasEl.height / window.devicePixelRatio + "px";
+	myCanvasEl.style.height = myCanvasEl.height / window.devicePixelRatio + "px";
 	myCanvasEl.style.width = myCanvasEl.width / window.devicePixelRatio + "px";
 	ctx.moveTo(l + block.offsetWidth / 2, t + block.offsetHeight / 2);
 	if (keys["d"]) {
