f618b626e07cc91f650b24bdd98aeb0987e4ea37
MagicToasterMan <88402889+eclar971@users.noreply.github.com>
2022-04-08 13:17:10 -0700

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+var pickerData;
+var startInterval;
 var blockLeft = 0;
 var blockTop = 0;
 var keys;
@@ -7,19 +9,169 @@ var topp = true;
 var shift = false;
 var random = false;
 var cornerTouch = 0;
-var interval = 2;
 var moveLeft = 1;
 var moveUp = 1;
 var lineColor = "red";
 var lineColorShift = false;
 var down = false;
 var first = true;
+var ifirst = false;
 var mouseColor;
 var pick = false;
 var strokeSize = 1;
-var globalImg;
+var globalImg = 0;
 var imageDropped = false;
+var i = 0;
+var j = 0;
+var startY = window.innerHeight / 2;
+var startX = window.innerWidth / 2;
+var ratio;
 
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+}
+function getRandomColor() {
+  var letters = "012356789ABCDEF";
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+function interval() {
+  startInterval = setInterval(function () {
+    if (imageDropped) {
+      ctx = document.getElementById("myCanvas").getContext("2d");
+      if (ifirst) {
+        data = ctx.getImageData(0, 0, globalImg.width, globalImg.height).data;
+        ctx.fillStyle = "#121212";
+        ctx.fillRect(0, 0, globalImg.width, globalImg.height);
+        startX = window.innerWidth / 2;
+        startY = window.innerHeight / 2;
+        j = 0;
+        ifirst = false;
+      }
+      if (j < data.length) {
+        block = document.getElementById("block");
+        block.style.top = startY - globalImg.height / 2 + "px";
+        block.style.left = startX - globalImg.width / 2 + "px";
+        currentPixel = "rgb(" + data[j];
+        j++;
+        currentPixel += ", " + data[j];
+        j++;
+        currentPixel += ", " + data[j] + ")";
+        j++;
+        ctx.beginPath();
+        ctx.moveTo(startX - globalImg.width / 2, startY - globalImg.height / 2);
+        startX++;
+        ctx.lineTo(startX - globalImg.width / 2, startY - globalImg.height / 2);
+        ctx.strokeStyle = currentPixel;
+        ctx.lineWidth = 1;
+        ctx.stroke();
+        j++;
+      } else {
+        clearInterval(startIntervals);
+      }
+      if (
+        startX - globalImg.width / 2 ==
+        globalImg.width + window.innerWidth / 2 - globalImg.width / 2
+      ) {
+        startY++;
+        startX = window.innerWidth / 2;
+      }
+    }
+    if (shift) {
+      const rightMost = window.innerWidth - block.offsetWidth - 16;
+      const leftMost = -8;
+      const highest = -8;
+      const lowest = window.innerHeight - block.offsetHeight - 14;
+      l = blockLeft;
+      t = blockTop;
+      if (line) {
+        var myCanvasEl = document.getElementById("myCanvas");
+        var ctx = myCanvasEl.getContext("2d");
+        ctx.beginPath();
+        myCanvasEl.style.width = window.innerWidth;
+        myCanvasEl.style.height = window.innerHeight;
+        ctx.moveTo(l + block.offsetWidth / 2, t + block.offsetHeight / 2);
+      }
+      if (left) {
+        if (blockLeft <= rightMost) {
+          blockLeft += moveLeft;
+          block.style.left = blockLeft + "px";
+        } else {
+          left = false;
+          color = getRandomColor();
+          block.style.color = color;
+          if (lineColorShift) {
+            lineColor = color;
+          }
+          if (random) {
+            moveLeft = getRandomIntInclusive(1, 5);
+          }
+        }
+      }
+      if (!left) {
+        if (blockLeft >= leftMost) {
+          blockLeft -= moveLeft;
+          block.style.left = blockLeft + "px";
+        } else {
+          left = true;
+          color = getRandomColor();
+          block.style.color = color;
+          if (lineColorShift) {
+            lineColor = color;
+          }
+          if (random) {
+            moveLeft = getRandomIntInclusive(1, 5);
+          }
+        }
+      }
+      if (topp) {
+        if (blockTop <= lowest) {
+          blockTop += moveUp;
+          block.style.top = blockTop + "px";
+        } else {
+          topp = false;
+          color = getRandomColor();
+          block.style.color = color;
+          if (lineColorShift) {
+            lineColor = color;
+          }
+          if (random) {
+            moveUp = getRandomIntInclusive(1, 5);
+          }
+        }
+      }
+      if (!topp) {
+        if (blockTop >= highest) {
+          blockTop -= moveUp;
+          block.style.top = blockTop + "px";
+        } else {
+          topp = true;
+          color = getRandomColor();
+          block.style.color = color;
+          if (lineColorShift) {
+            lineColor = color;
+          }
+          if (random) {
+            moveUp = getRandomIntInclusive(1, 5);
+          }
+        }
+      }
+      if (line) {
+        ctx.lineTo(
+          blockLeft + block.offsetWidth / 2,
+          blockTop + block.offsetHeight / 2
+        );
+        ctx.strokeStyle = lineColor;
+        ctx.stroke();
+      }
+    }
+  }, 4);
+}
 window.addEventListener("resize", function () {
   var myCanvasEl = document.getElementById("myCanvas");
   myCanvasEl.style.width = window.innerWidth;
@@ -39,13 +191,8 @@ document.addEventListener("dragover", function (e) {
   e.preventDefault();
   document.getElementById("dropZone").style.backgroundColor = "red";
 });
-var i = 0;
-var j = 0;
-var startY = window.innerHeight / 2;
-var startX = window.innerWidth / 2;
 window.addEventListener("drop", function (ev) {
   ev.preventDefault();
-  imageDropped = true;
   document.getElementById("dropZone").style.backgroundColor = "black";
   console.log("File(s) dropped");
 
@@ -72,10 +219,21 @@ window.addEventListener("drop", function (ev) {
   var url = URL.createObjectURL(file);
   var img = new Image();
   img.onload = function () {
-    ctx.drawImage(img, 0, 0);
+    ratio = img.width/img.height
+    img.width = 250
+    img.height = 250*ratio
+    ctx.drawImage(img, 0, 0, img.width, img.height);
   };
   img.src = url;
+  startX = window.innerWidth / 2;
+  startY = window.innerHeight / 2;
+  j = 0;
   globalImg = img;
+  ctx.fillStyle = "#121212";
+  ctx.fillRect(0, 0, img.width, img.height);
+  imageDropped = true;
+  ifirst = true;
+  interval();
 });
 window.addEventListener("mousedown", function () {
   down = true;
@@ -109,135 +267,6 @@ window.addEventListener("mousedown", function () {
 window.addEventListener("mouseup", function () {
   down = false;
 });
-function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
-}
-function getRandomColor() {
-  var letters = "012356789ABCDEF";
-  var color = "#";
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-}
-setInterval(function () {
-  if (imageDropped) {
-    if (j > globalImg.height) {
-      startX++;
-      i++;
-      j = 0;
-      startY = window.innerHeight / 2;
-    } else if (i > globalImg.width) {
-      imageDropped = false;
-    }
-    ctx = document.getElementById("myCanvas").getContext("2d");
-    if (j < globalImg.height) {
-      block = document.getElementById("block");
-      block.style.top = startY + "px";
-      block.style.left = startX + "px";
-      data = ctx.getImageData(i, j, 1, 1).data;
-      currentPixel = "rgb(" + data[0] + ", " + data[1] + ", " + data[2] + ")";
-      ctx.beginPath();
-      ctx.moveTo(startX, startY);
-      startY++;
-      ctx.lineTo(startX, startY);
-      ctx.strokeStyle = currentPixel;
-      ctx.lineWidth = 1;
-      ctx.stroke();
-    }
-    j++;
-  }
-  if (shift) {
-    const rightMost = window.innerWidth - block.offsetWidth - 16;
-    const leftMost = -8;
-    const highest = -8;
-    const lowest = window.innerHeight - block.offsetHeight - 14;
-    l = blockLeft;
-    t = blockTop;
-    if (line) {
-      var myCanvasEl = document.getElementById("myCanvas");
-      var ctx = myCanvasEl.getContext("2d");
-      ctx.beginPath();
-      myCanvasEl.style.width = window.innerWidth;
-      myCanvasEl.style.height = window.innerHeight;
-      ctx.moveTo(l + block.offsetWidth / 2, t + block.offsetHeight / 2);
-    }
-    if (left) {
-      if (blockLeft <= rightMost) {
-        blockLeft += moveLeft;
-        block.style.left = blockLeft + "px";
-      } else {
-        left = false;
-        color = getRandomColor();
-        block.style.color = color;
-        if (lineColorShift) {
-          lineColor = color;
-        }
-        if (random) {
-          moveLeft = getRandomIntInclusive(1, 5);
-        }
-      }
-    }
-    if (!left) {
-      if (blockLeft >= leftMost) {
-        blockLeft -= moveLeft;
-        block.style.left = blockLeft + "px";
-      } else {
-        left = true;
-        color = getRandomColor();
-        block.style.color = color;
-        if (lineColorShift) {
-          lineColor = color;
-        }
-        if (random) {
-          moveLeft = getRandomIntInclusive(1, 5);
-        }
-      }
-    }
-    if (topp) {
-      if (blockTop <= lowest) {
-        blockTop += moveUp;
-        block.style.top = blockTop + "px";
-      } else {
-        topp = false;
-        color = getRandomColor();
-        block.style.color = color;
-        if (lineColorShift) {
-          lineColor = color;
-        }
-        if (random) {
-          moveUp = getRandomIntInclusive(1, 5);
-        }
-      }
-    }
-    if (!topp) {
-      if (blockTop >= highest) {
-        blockTop -= moveUp;
-        block.style.top = blockTop + "px";
-      } else {
-        topp = true;
-        color = getRandomColor();
-        block.style.color = color;
-        if (lineColorShift) {
-          lineColor = color;
-        }
-        if (random) {
-          moveUp = getRandomIntInclusive(1, 5);
-        }
-      }
-    }
-    if (line) {
-      ctx.lineTo(
-        blockLeft + block.offsetWidth / 2,
-        blockTop + block.offsetHeight / 2
-      );
-      ctx.strokeStyle = lineColor;
-      ctx.stroke();
-    }
-  }
-}, 4);
 window.addEventListener("wheel", function (event) {
   if (event.wheelDeltaY > 0) {
     strokeSize += 1;
@@ -259,19 +288,22 @@ window.addEventListener("keydown", function movement(obj) {
       block.style.left = blockLeft + "px";
     }
   }
+  if (keys["e"]){
+    mouseColor = "#121212"
+  }
   if (keys["p"]) {
     img = document.getElementById("colorWheel");
     ctx.drawImage(img, 0, 0);
     function picker(event) {
       mousex = event.clientX;
       mousey = event.clientY;
-      data = ctx.getImageData(mousex, mousey, 1, 1).data;
+      pickerData = ctx.getImageData(mousex, mousey, 1, 1).data;
       mouseColor = "rgb(" + data[0] + ", " + data[1] + ", " + data[2] + ")";
       ctx.fillStyle = "#121212";
       ctx.fillRect(0, 0, img.naturalWidth, img.naturalHeight);
       window.removeEventListener("mousedown", picker);
     }
-    window.addEventListener("mousedown", picker);
+    window.addEventListener("mousedown", picker );
   }
   if (keys["w"]) {
     if (blockTop >= -8) {
@@ -314,8 +346,10 @@ window.addEventListener("keydown", function movement(obj) {
   if (keys["Shift"]) {
     if (!shift) {
       shift = true;
+      interval()
     } else {
       shift = false;
+      clearTimeout(startInterval)
     }
   }
   if (keys["]"]) {
